1135be00e15f55ac224e8fdcf74daae29e8fd65d
Rocky Neurock <jneurock@gmail.com>
2020-08-08 23:47:37 +0200

    Add a build to the project for npm

--- index.js
+++
@@ -1,4 +0,0 @@
-import { createGraphQLHandler } from "./lib/handler";
-import mirageGraphQLFieldResolver from "./lib/resolvers/mirage";
-
-export { createGraphQLHandler, mirageGraphQLFieldResolver };
