4070463a78cd9b339ab28715b391309959a0d456
Rocky Neurock <jneurock@gmail.com>
2020-10-05 21:55:00 +0200

    Fix getting variable types in auto-mutation resolver (#21)

--- __tests__/integration/mutations/update-object-test.js
+++ __tests__/integration/mutations/update-object-test.js
@@ -1,10 +1,19 @@
 import updateTestObjectMutation from "@tests/gql/mutations/update.gql";
+import updateTestObjectNonNullMutation from "@tests/gql/mutations/update-non-null.gql";
 import { mutate, startServer } from "@tests/integration/setup";
 
-describe("Integration | mutations | create", function () {
-  it("can create a test object", async function () {
-    const server = startServer();
+let server;
 
+describe("Integration | mutations | create", function () {
+  beforeEach(function () {
+    server = startServer();
+  });
+
+  afterEach(function () {
+    server.shutdown();
+  });
+
+  it("can update a test object", async function () {
     server.create("test-object", { size: "M" });
 
     const { updateTestObject } = await mutate(updateTestObjectMutation, {
@@ -13,6 +22,21 @@ describe("Integration | mutations | create", function () {
     const record = server.schema.testObjects.first();
 
     expect(updateTestObject).toEqual({ id: "1", size: "L" });
+    expect(record.size).toBe("L");
+  });
+
+  it("can update a test object (non-null input)", async function () {
+    server.create("test-object", { size: "M" });
+
+    const { updateTestObjectNonNull } = await mutate(
+      updateTestObjectNonNullMutation,
+      {
+        variables: { id: "1", input: { size: "L" } },
+      }
+    );
+    const record = server.schema.testObjects.first();
+
+    expect(updateTestObjectNonNull).toEqual({ id: "1", size: "L" });
     expect(record.size).toBe("L");
   });
 });
